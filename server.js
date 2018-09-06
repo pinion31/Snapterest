182c3c0d0b0783edb55028d37b1fad1410fcaf75
Chris Cantu <pinion31@gmail.com>
2018-09-06 13:29:52 -0500

    added cors module

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'https://sparkle-app.herokuapp.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 const appRoutes = require('./src/routes/app');
 const requireAuth = require('./server/auth');
@@ -18,15 +24,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(__dirname + '/dist/sparkle'));
 
-
+/*
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'https://sparkle-app.herokuapp.com');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
   next();
 });
-
-app.use('/', requireAuth, appRoutes);
+*/
+app.use('/', cors(corsOptions), requireAuth, appRoutes);
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/sparkle/index.html'));
