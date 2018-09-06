b86a2e7d3eac07edcd482e53f29ea58f358e5f5f
Chris Cantu <pinion31@gmail.com>
2018-09-06 12:59:06 -0500

    Removed CORS

--- server.js
+++ server.js
@@ -18,12 +18,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(__dirname + '/dist/sparkle'));
 
+/*
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
   next();
-});
+});*/
 
 app.use('/', requireAuth, appRoutes);
 
